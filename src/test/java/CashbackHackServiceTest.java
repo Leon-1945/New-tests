faa3e31bb17399ac888f31770d5a7bcc9dc458f9
Leon1945 <Muravlevevgeniy@yandex.ru>
2023-04-06 23:54:05 +0400

    next2

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,6 +1,31 @@
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
-
+    @Test
+    public void testRemain() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testRemain2() {
+        int amount = 990;
+        int expected = 10;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testRemain3() {
+        int amount = 1000;
+        int expected = 1000;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
 }
